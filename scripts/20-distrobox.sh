9a57193582c4c6f8fe9bcb09ec8d9540630642ff
kdawson133 <kdawson133@gmail.com>
2024-08-14 10:17:09 +1000

    updated

--- scripts/20-distrobox.sh
+++ scripts/20-distrobox.sh
@@ -2,3 +2,4 @@
 # Installing Prerequisites
 sudo xbps-install podman wget
 wget -qO- https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
+echo 'unqualified-search-registries = ["docker.io"]' | sudo tee -a /etc/containers/registries.conf
