1993f2ab9ecda406e603e84a3ce7b52671e864c3
kdawson133 <kdawson133@startmail.com>
2026-08-15 09:52:51 +1000

    updated

--- scripts/05-niri.sh
+++ scripts/05-niri.sh
@@ -16,7 +16,7 @@ sudo xbps-install vim neovim dbus elogind polkit xorg xorg-fonts xorg-input-driv
 sudo xbps-install curl wget wl-clipboard xsel xclip nodejs bat eza base-devel helix starship zsh stow fd ripgrep fzf zoxide
 
 # Install Desktop Packages
-sudo xbps-install niri noctalia-shell fuzzel alacritty nvidia gnome-keyring polkit-gnome fastfetch firefox 
+sudo xbps-install niri noctalia-shell fuzzel alacritty nvidia gnome-keyring polkit-gnome fastfetch xwayland-satellite firefox 
 
 # Install Fonts
 sudo xbps-install font-Hasklig nerd-fonts-otf nerd-fonts-symbols-ttf
